659d6a192cf36e094c04e9806a909bf143ca1c44
AlexSh <nivaland@yandex.ru>
2017-01-07 05:33:21 +0300

    +some changes in Pagination Tests

--- src/test/java/org/mytests/MetalAndColorsPaginationTests.java
+++ src/test/java/org/mytests/MetalAndColorsPaginationTests.java
@@ -15,32 +15,7 @@ import static org.mytests.EpamTestSite.*;
  * Created by AlexSh on 20.11.2016.
  */
 
-/*
 
-@JPage(url = "/index.htm", title = "Index Page", urlCheckType = CONTAINS, titleCheckType= CONTAINS)
-public static HomePage homePage;
-
-@JPage(url = "/page2.htm", title = "Metal and Colors", urlCheckType = CONTAINS, titleCheckType= CONTAINS)
-public static MetalsAndColorsPage metalsAndColorsPage;
-
-@JPage(url = "/page8.htm", title = "Different Element ", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static DifferentElementsPage differentElementsPage;
-
-@JPage(url = "/page1.htm", title = "Contact Form", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static ContactFormPage contactFormPage;
-
-@JPage(url = "/page3.htm", title = "Support", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static SupportPage supportPage;
-
-@JPage(url = "/page4.htm", title = "Dates", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static DatesPage datesPage;
-
-@JPage(url = "/page6.htm", title = "Simple Table", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static SimpleTablePage simpleTablePage;
-
-@JPage(url = "/page7.htm", title = "Table with Pages ", urlCheckType = CONTAINS, titleCheckType= CONTAINS )
-public static TableWithPagesPage tableWithPagesPage;
-*/
 
 public class MetalAndColorsPaginationTests extends InitTests{
 
